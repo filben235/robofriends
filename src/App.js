72c54cd3088634c73e7b6068862c59c27cd77cf2
Filip Bengs <filip.bengs@netikka.fi>
2020-10-01 16:11:02 +0300

    build-react-app 4

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import CardList from './CardList';
-import { robots } from './robots';
 import SearchBox from './SearchBox';
 import './App.css';
 
@@ -9,9 +8,15 @@ class App extends Component {
    constructor() {
       super()
       this.state = {
-         robots: robots,
+         robots: [],
          searchfield: ''
       }
+   }
+
+   componentDidMount() {
+      fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response => response.json())
+      .then(users => this.setState({ robots: users }));
    }
 
    onSearchChange = (event) => {
@@ -19,20 +24,22 @@ class App extends Component {
    }
 
 
-
    render() {
-      const filteredRobots = this.state.robots.filter(robots => {
-         return robots.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
-      })
+   const filteredRobots = this.state.robots.filter(robots => {
+      return robots.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
+   })
+   if (this.state.robots.length === 0){
+      return <h1>Loading</h1>
+   } else {
       return(
          <div className='tc'>
             <h1 className='f1'>RoboFriends</h1>
             <SearchBox searchChange={this.onSearchChange}/>
             <CardList robots={filteredRobots}/>
          </div>
-      );
+         );
+      }
    }
-}
-   
+}   
 
 export default App;
