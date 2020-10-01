ac58e9bd45ea66ff108c8112403c74ea2bfe9fac
Filip Bengs <filip.bengs@netikka.fi>
2020-10-01 15:44:56 +0300

    build-react-app-3

--- src/CardList.js
+++ src/CardList.js
@@ -7,7 +7,7 @@ const CardList = ({ robots }) => {
          {
             robots.map((user, i) => {
                return ( 
-               <Card 
+                  <Card 
                   key={i} 
                   id={robots[i].id} 
                   name={robots[i].name} 
